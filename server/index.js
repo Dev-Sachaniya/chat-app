df70520b6608e32a20188c83bf9324b5db6e9ada
Dev-Sachaniya <127300562+Dev-Sachaniya@users.noreply.github.com>
2023-08-28 11:42:15 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://chat-app-client-zeta.vercel.app/",
+    origin: "https://chat-app-client-zeta.vercel.app",
     methods: ["GET", "POST"],
   },
 });
